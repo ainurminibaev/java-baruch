9b6c2756135d0f49a062370608bc631d3e8680fc
Ainur Minibaev <rapper01@list.ru>
2015-04-26 16:15:22 +0300

    JBC-2 fix #JBC-2 Closed

--- src/main/java/foo/bar/HelloApp.java
+++ src/main/java/foo/bar/HelloApp.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloApp {
     public static void main(String[] args) {
-        //leaving comment
+        //leaving comment opop
         ApplicationContext context = new ClassPathXmlApplicationContext("day01/spring-config.xml");
         HelloService helloService = context.getBean(HelloService.class);
         System.out.println(helloService.sayHello());
